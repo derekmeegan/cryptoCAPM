81a7ec07c13ceaf86ad4cdb75d6fa48c3f650cdd
derekmeegan <derek.j.meegan@gmail.com>
2021-05-17 10:57:03 -0700

    continuing analysis

--- model.R
+++ model.R
@@ -9,37 +9,64 @@ library(quantmod)
 library(tidyverse)
 library(ggdark)
 library(scales)
-
-getSymbols("DGS3MO", src = "FRED")
+library(PerformanceAnalytics)
 
 #Visualize 3 Month Treasury Returns 
+getSymbols("DGS3MO", src = "FRED")
 
 DGS3MO %>% 
   chartSeries(name = "3 Month Treasury Returns")
 
 #Get Annual Period Returns for 3 Month Treasury
-
 annualReturns <- DGS3MO %>% 
   na.omit %>% 
   periodReturn(period = "yearly") %>% 
-  as.tibble
-
+  .[-c(length(.)),]
 
 annualReturns %>% ggplot(aes(x = index(annualReturns), y= yearly.returns)) +
-  geom_line(color = "lightGreen") +
+  geom_col(color = "lightGreen") +
   geom_smooth(method="loess", color="yellow") +
   dark_mode() + 
   ggtitle("3 Month Treasury Period Returns (Annual)") +
   scale_y_continuous(labels = percent) +
-  labs(
-    x = "Returns", 
-    y = "Year")
+  labs(x = "Year", y = "Returns")
 
 #Current Risk Free Rate
-
 rF <- DGS3MO %>% 
   as.tibble %>% 
   tail(1) %>% 
   pull
 
+#Get Crypto Market Caps
+cryptoData <- read.csv(file = "data.csv")
 
+getAnnualReturns <- function(symbols) {
+  answer <- c()
+  for (symbol in symbols) {
+    answer[[symbol]] <- getSymbols(gsub(" ", "", paste(symbol, "-usd")), auto.assign = FALSE) %>% 
+      Cl %>% 
+      periodReturn('yearly') %>% 
+      as.tibble %>% 
+      pull %>% 
+      mean
+  }
+   return(answer)
+}
+
+getAnnualReturns("btc") %>% tail
+
+df <- cryptoData %>% 
+  as.tibble %>% 
+  .[c(2,3,5)] %>% 
+  arrange(market_cap %>% desc) %>% 
+  head(50) 
+
+write.table(df , file = "top50_mktcap.csv")
+
+
+portfolio_returns_xts_rebalanced_monthly <- 
+  Return.portfolio(asset_returns_xts, weights = w, rebalance_on = "months") %>%
+  `colnames<-`("returns")
+
+
+  
